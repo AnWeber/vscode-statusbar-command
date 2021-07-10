29b82c9e7d04ac1443b4edca8e68b8bde51acdc0
Andreas Weber <weber.andreas@gmail.com>
2021-07-10 19:51:26 +0200

    fix: forgotten line removed

--- src/statusBarCommand.ts
+++ src/statusBarCommand.ts
@@ -80,7 +80,6 @@ export class StatusBarCommand {
     this.statusBarItem.name = config.name;
     this.statusBarItem.text = config.text;
     this.statusBarItem.tooltip = config.tooltip;
-    this.statusBarItem.tooltip
     if (config.backgroundColor) {
       this.statusBarItem.backgroundColor = new vscode.ThemeColor(config.backgroundColor);
     }
