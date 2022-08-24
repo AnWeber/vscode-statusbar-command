6805cb60f2707dc866a10bdf00bc2d43a2787391
Andreas Weber <weber.andreas@gmail.com>
2022-08-24 21:29:39 +0200

    feat: allow require in trusted workspace #21

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,26 @@
 import * as vscode from 'vscode';
 import { CommandsController } from './commandsController';
-import { runInNewContext } from 'vm';
+import * as vm from 'vm';
 
 
 export function activate(context: vscode.ExtensionContext): void {
 
-  context.subscriptions.push(new CommandsController(runInNewContext));
+  context.subscriptions.push(new CommandsController((script: string, context: Record<string, unknown>) => {
+    if (vscode.workspace.isTrusted) {
+      vm.runInContext(script, vm.createContext(
+        Object.assign(
+          Object.defineProperties(
+            {
+              ...global,
+            },
+            Object.getOwnPropertyDescriptors(global),
+          ),
+          { vscode },
+          context,
+        )
+      ));
+    } else {
+      vm.runInNewContext(script, context);
+    }
+  }));
 }
